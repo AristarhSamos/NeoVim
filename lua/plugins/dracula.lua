c75bf6fea151581f7e397a33846b420131f10d43
AristarhSamos <utiakovskiy@yandex.ru>
2024-06-02 17:40:57 +0300

    Сделал темную тему Dracula и пару плагинов

---
+++ lua/plugins/dracula.lua
@@ -0,0 +1,14 @@
+-- return {
+--     -- add dracula
+--     { "Mofiqul/dracula.nvim" },
+
+--     -- Configure LazyVim to load dracula
+--     {
+--       "LazyVim/LazyVim",
+--       opts = {
+--         colorscheme = "dracula-soft",
+--       },
+--     },
+--   }
+
+vim.cmd[[colorscheme dracula-soft]]
